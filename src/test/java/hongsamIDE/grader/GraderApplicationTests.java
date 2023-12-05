eb151c0d955da600fbf5e856c4d894ff62b7aed8
woo <ldw28517@naver.com>
2023-12-05 17:38:28 +0900

    init

---
+++ src/test/java/hongsamIDE/grader/GraderApplicationTests.java
@@ -0,0 +1,13 @@
+package hongsamIDE.grader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
